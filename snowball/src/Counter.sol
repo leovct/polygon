d1e7d6de1c1f8ca692b45840b351197de3d0ec97
leovct <l009.vincent@gmail.com>
2023-09-13 11:36:09 +0200

    chore: lint

--- snowball/src/Counter.sol
+++ snowball/src/Counter.sol
@@ -2,26 +2,30 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
+  uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+  function setNumber(uint256 newNumber) public {
+    number = newNumber;
+  }
 
-    function increment() public returns(uint256) {
-        number++;
-        return number;
-    }
+  function increment() public returns (uint256) {
+    number++;
+    return number;
+  }
 
-    function tryRevert() public {
-        number++;
-        revert();
+  function tryRevert() public {
+    number++;
+    revert();
+  }
+
+  function terminate(address payable addr) public {
+    selfdestruct(addr);
+  }
+
+  function stop() public {
+    number++;
+    assembly {
+      stop()
     }
-    function terminate(address payable addr) public {
-        selfdestruct(addr);
-    }
-    function stop() public {
-        number++;
-        assembly {stop()}
-    }
+  }
 }
