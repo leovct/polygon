07bf7b8655a58474f035f7ba9d46d8c839fb4ea7
leovct <l009.vincent@gmail.com>
2024-01-17 13:48:19 +0100

    fix: bench script

--- ops/benchmark/bench.sh
+++ ops/benchmark/bench.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-MEMLATENCY_BINARY=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/memory-latency/memlatency
-POSEIDONHASH_BINARY=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/poseidon-hashes/poseidonhash
-PROVER_BINARY=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/proof/prover
-WITNESS_URL=http://jhilliard-zero-highmem.hardfork.dev/0x2f0faea6778845b02f9faf84e7e911ef12c287ce7deb924c5925f3626c77906e.json.bz2
-MICROBENCH_SCRIPT=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/microbench.sh
+MEMLATENCY_BINARY_URL=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/memory-latency/memlatency
+POSEIDONHASH_BINARY_URL=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/poseidon-hashes/poseidonhash
+PROVER_BINARY_ARCHIVE_URL=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/proof/prover.tar.bz2
+WITNESS_ARCHIVE_URL=http://jhilliard-zero-highmem.hardfork.dev/0x2f0faea6778845b02f9faf84e7e911ef12c287ce7deb924c5925f3626c77906e.json.bz2
+MICROBENCH_SCRIPT_URL=https://raw.githubusercontent.com/leovct/polygon/feat/benchmarks/ops/benchmark/microbench.sh
 
 function bench {
   name=$1
   instance=$2
   echo "Running benchmark script on $name..."
   gcloud compute ssh --zone "$ZONE" "$instance" --project "$PROJECT" -- \
-    "sudo curl -L $MEMLATENCY_BINARY --output /usr/bin/memlatency \
+    "sudo curl -L $MEMLATENCY_BINARY_URL --output /usr/bin/memlatency \
       && sudo chmod +x /usr/bin/memlatency \
-      && sudo curl -L $POSEIDONHASH_BINARY --output /usr/bin/poseidonhash \
+      && sudo curl -L $POSEIDONHASH_BINARY_URL --output /usr/bin/poseidonhash \
       && sudo chmod +x /usr/bin/poseidonhash \
-      && sudo curl -L $PROVER_BINARY --output /usr/bin/prover \
-      && sudo chmod +x /usr/bin/prover \
-      && curl -OJL $WITNESS_URL \
       && sudo apt-get install bzip2 --yes \
-      && bzip2 -d $(basename $WITNESS_URL) \
-      && curl -L $MICROBENCH_SCRIPT | bash" > results/$name.bench 2>&1 &
+      && sudo curl -OJL $PROVER_BINARY_ARCHIVE_URL \
+      && bzip2 -d $(basename $PROVER_BINARY_ARCHIVE_URL) \
+      && sudo chmod +x /usr/bin/prover \
+      && sudo mv prover /usr/bin/prover \
+      && curl -OJL $WITNESS_ARCHIVE_URL \
+      && bzip2 -d $(basename $WITNESS_ARCHIVE_URL) \
+      && curl -L $MICROBENCH_SCRIPT_URL | bash" > results/$name.bench 2>&1 &
 }
 
 # Load variables.
@@ -37,8 +39,7 @@ echo
 # Run benchmarks in the background.
 rm -rf ./results
 mkdir -p results
-bench "c3d-standard-8" "leovct-bench-test-01"
-bench "t2d-standard-8" "leovct-bench-test-02"
+bench "t2d-standard-16" "leovct-bench-test-03"
 
 # Wait for all background processes to finish
 wait
